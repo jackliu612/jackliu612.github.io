75646d6510d0695daca944e08afb8def0f426f88
jackliu612 <jackliu612@gmail.com>
2022-04-22 10:31:15 -0400

    adding more scroll effects and jump to top button

--- scripts/index.js
+++ scripts/index.js
@@ -6,13 +6,28 @@ function hideResponsiveBlocker(){
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop() < $(window).height()/2){
-            $("canvas").css({"opacity" : "20%"})
+            $("canvas").css({"opacity" : ".2"});
+            $(".main").css({"opacity" : "1"});
         }
         else{
-            $("canvas").css({"opacity" : ($(this).scrollTop()-$(window).height()/2) / ($(window).height()/2)*2+.2})
+            $("canvas").css({"opacity" : ($(this).scrollTop()-$(window).height()/2) / ($(window).height()/2)*2+.2});
+            $(".main").css({"opacity" : 1-($(this).scrollTop()-$(window).height()/2) / ($(window).height()/2)*3})
         }
         // else {
         //     $("canvas").css({"opacity" : "1"})
         // }
-    })
-  })
+    });
+
+    $('#scrollToTop').on('click', function() {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
+
+});
+
+$(window).on("scroll", function () {
+    if ($(this).scrollTop() > 2*$(window).height()/3) {
+        $("#scrollToTop").addClass("active");
+    } else {
+        $("#scrollToTop").removeClass("active");
+    }
+});
